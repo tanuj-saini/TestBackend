d725ae38ff92158db64e5927e9a37d83f9fb8d87
Tanuj Saini <tanujsaini97999@gamil.com>
2025-01-11 21:31:44 +0530

    "Changes"

--- src/index.js
+++ src/index.js
@@ -4,27 +4,6 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 dotenv.config();
 const app = express();
-
-
 connectDB();
-
-// ;(async ()=>{
-//   try{
-//     await mongoose.connect(DB)
-//     app.on("error", (err) => {
-//       console.error("ERROR:", err);
-//       throw err;
-//     });
-//     app.listen(currentPORT, "0.0.0.0", () => {
-//       console.log(`connected at port ${currentPORT}`);
-//     });
-    
-    
-
-//   }catch(e){
-//     console.error("ERROR:",e)
-//     throw e
-//   }
-// })()
 
 

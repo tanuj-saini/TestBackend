794c1a9c1697193c7ff68067eafbf42e5ff8fc90
Tanuj Saini <tanujsaini97999@gamil.com>
2025-01-11 22:06:25 +0530

    "Changes on Port "

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-import express from "express";
+
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config();
-const app = express();
-connectDB();
 
+const PORT = process.env.PORT || 3000;
+
+connectDB().then(
+    () => {
+      app.listen(PORT, () => {
+        console.log("Server is running on port " + PORT);
+      })
+    }
+).catch((e) => {
+    console.error("ERROR:", e);
+    process.exit(1);
+})
 

70cc9ef5a74f0eaa141559b5f86e1d77a0a04dd3
Tanuj Saini <tanujsaini97999@gamil.com>
2025-01-11 19:51:46 +0530

    "Changes"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-require("dotenv").config();
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
-const currentPORT = process.env.PORT;
+const currentPORT = process.env.PORT || 8080;
 
 app.get("/hello", (req, res) => {
   res.send("hello");
 });
+app.get("/king", (req, res) => {});
 
 app.listen(currentPORT, "0.0.0.0", () => {
   console.log(`connected at port ${currentPORT}`);

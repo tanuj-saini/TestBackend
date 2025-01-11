740222bb5842719c130cfe359f76cfa8e2d713b2
Tanuj Saini <tanujsaini97999@gamil.com>
2025-01-11 17:00:37 +0530

    "Changes"

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const currentPORT = process.env.PORT;
 
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
 app.listen(currentPORT, "0.0.0.0", () => {
   console.log(`connected at port ${currentPORT}`);
 });

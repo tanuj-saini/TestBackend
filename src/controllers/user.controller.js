1391cf0ad5756de5d2e722ee4160a55c9c9980d6
Tanuj Saini <tanujsaini97999@gamil.com>
2025-01-12 13:31:26 +0530

    "Error Changes"

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,12 +14,9 @@ const registerUser = asyncHandler(async (req, res) => {
     if(existUser){
         throw new ApiError(409,"User already exists");
     }
-     const avatarLocalPath= req.files?.avatar[0]?.path;
-   //  const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
-   let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        coverImageLocalPath = req.files.coverImage[0].path
-    }
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+     const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
+   
     
 
      console.log(avatarLocalPath,coverImageLocalPath);
